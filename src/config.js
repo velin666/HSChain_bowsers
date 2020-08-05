fcc96ab83d052f3a4adf696e58ad645ea847fa8c
wuyulin <786168197@qq.com>
2020-08-05 17:17:24 +0800

    wyl: 新增项目内容

---
+++ src/config.js
@@ -0,0 +1,10 @@
+/*
+ * enter backend apis
+ */
+
+const config = Object.freeze({
+	API_DEV: "https://api-binance-testnet.cosmostation.io/v1",
+	API_PROD: "https://api-binance-mainnet.cosmostation.io/v1",
+});
+
+export default config;
